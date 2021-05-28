b45d55005ef0753f2c520e26a1045a26cad23966
LWooler <lwooler@att.net>
2021-05-28 16:34:23 -0500

    merged DTO models with main

---
+++ CarDealerApiService.App/models/VehiclePriceDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CarDealerAPIService.App.models
+{
+  public class VehiclePriceDTO
+  {
+    [JsonProperty("vin")] public string Vin { get; set; }
+
+    [JsonProperty("success")] public bool Success { get; set; }
+
+    [JsonProperty("vehicle")] public string Vehicle { get; set; }
+
+    [JsonProperty("mileage")] public long Mileage { get; set; }
+
+    [JsonProperty("count")] public long Count { get; set; }
+
+    [JsonProperty("mean")] public long Mean { get; set; }
+
+    [JsonProperty("stdev")] public long Stdev { get; set; }
+
+    [JsonProperty("certainty")] public long Certainty { get; set; }
+
+    [JsonProperty("period")] public List<DateTimeOffset> Period { get; set; }
+
+    [JsonProperty("prices")] public Prices Prices { get; set; }
+  }
+
+  public class Prices
+  {
+    [JsonProperty("average")] public long Average { get; set; }
+
+    [JsonProperty("below")] public long Below { get; set; }
+
+    [JsonProperty("above")] public long Above { get; set; }
+  }
+}
