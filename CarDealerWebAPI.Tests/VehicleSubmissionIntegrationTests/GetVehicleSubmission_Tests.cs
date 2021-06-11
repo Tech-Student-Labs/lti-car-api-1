c85ae813f3452db64ea55458b16599a0e80c3075
Steven Pazur <sjpazur@yahoo.com>
2021-06-11 12:10:09 -0700

    fixed failing tests

--- CarDealerWebAPI.Tests/VehicleSubmissionIntegrationTests/GetVehicleSubmission_Tests.cs
+++ CarDealerWebAPI.Tests/VehicleSubmissionIntegrationTests/GetVehicleSubmission_Tests.cs
@@ -37,10 +37,14 @@ namespace CarDealerWebAPI.Tests.VehicleSubmissionIntegrationTests
             var databaseContext = new CarDealerContext(options);
             var vehicleSubmissionsService = new VehicleSubmissionsService(databaseContext);
             //When
+            User MyUser = new User() {Id = "abc123", Email = "string@string.com", PasswordHash = "xxxxxx"};
+            Vehicle vehicle = new Vehicle() {Id = 1};
+            databaseContext.UserTable.Add(MyUser);
+            databaseContext.VehicleInventory.Add(vehicle);
             var submission = new VehicleSubmissions() {
-                UserId = "abc123",
+                User = MyUser,
                 TimeStamp = new DateTime(12, 12, 12),
-                VehicleId = 1
+                Vehicle = vehicle
             };
             databaseContext.VehicleSubmissions.Add(submission);
             databaseContext.SaveChanges();
@@ -60,20 +64,23 @@ namespace CarDealerWebAPI.Tests.VehicleSubmissionIntegrationTests
             var vehicleSubmissionsService = new VehicleSubmissionsService(databaseContext);
             //When
             User MyUser = new User() {Id="abc123"};
+            Vehicle vehicle1 = new Vehicle() {Id = 1, Make = "Toyota"};
+            Vehicle vehicle2 = new Vehicle() {Id = 2, Make = "Chevy"};
+            Vehicle vehicle3 = new Vehicle() {Id = 3, Make = "SUV"};
             var submission1 = new VehicleSubmissions() {
-                UserId = MyUser.Id,
+                User = MyUser,
                 TimeStamp = new DateTime(12, 12, 12),
-                VehicleId = 1
+                Vehicle = vehicle1
             };
             var submission2 = new VehicleSubmissions() {
-                UserId = MyUser.Id,
+                User = MyUser,
                 TimeStamp = new DateTime(12, 12, 12),
-                VehicleId = 2
+                Vehicle = vehicle2
             };
             var submission3 = new VehicleSubmissions() {
-                UserId = MyUser.Id,
+                User = MyUser,
                 TimeStamp = new DateTime(12, 12, 12),
-                VehicleId = 3
+                Vehicle = vehicle3
             };
             databaseContext.VehicleSubmissions.Add(submission1);
             databaseContext.VehicleSubmissions.Add(submission2);
